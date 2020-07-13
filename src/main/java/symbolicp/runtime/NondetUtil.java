a2ea66075f574f3e2dc40b122740b8e4575ad104
Pick <lpick@amazon.com>
2020-07-13 16:23:22 -0700

    Update nondet choice

--- src/main/java/symbolicp/runtime/NondetUtil.java
+++ src/main/java/symbolicp/runtime/NondetUtil.java
@@ -1,15 +1,10 @@
 package symbolicp.runtime;
 
 import symbolicp.bdd.Bdd;
-import symbolicp.util.Checks;
-import symbolicp.vs.BoolUtils;
+import symbolicp.util.ValueSummaryUnionFind;
 import symbolicp.vs.PrimVS;
-import symbolicp.vs.ValueSummary;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class NondetUtil {
 
@@ -35,8 +30,8 @@ public class NondetUtil {
         return res;
     }
 
-    public static PrimVS getNondetChoice(List<PrimVS> choices) {
-        if(choices.size() == 0) return new PrimVS<>();
+    public static PrimVS getNondetChoiceAlt(List<PrimVS> choices) {
+        if (choices.size() == 0) return new PrimVS<>();
         List<PrimVS> results = new ArrayList<>();
         PrimVS empty = choices.get(0).guard(Bdd.constFalse());
         List<Bdd> choiceVars = new ArrayList<>();
@@ -45,30 +40,81 @@ public class NondetUtil {
         while ((1 << numVars) - choices.size() < 0) {
             numVars++;
         }
-        int residual = (1 << numVars) - choices.size();
 
         for (int i = 0; i < numVars; i++) {
             choiceVars.add(Bdd.newVar());
         }
 
         List<Bdd> choiceConds = generateAllCombos(choiceVars);
-        assert(choiceConds.size() - residual == choices.size());
-        while (residual != 0) {
-            Bdd last = choiceConds.remove(choiceConds.size() - 1);
-            Bdd newLast = choiceConds.get(choiceConds.size() - 1);
-            choiceConds.set(choiceConds.size() - 1, newLast.or(last));
-            residual--;
-        }
-        assert(choices.size() == choiceConds.size());
 
         Bdd accountedPc = Bdd.constFalse();
         for (int i = 0; i < choices.size(); i++) {
             PrimVS choice = choices.get(i).guard(choiceConds.get(i));
             results.add(choice);
-            accountedPc = accountedPc.or(choice.getUniverse()); //and(choice.getUniverse().not());
+            accountedPc = accountedPc.or(choice.getUniverse());
         }
 
         Bdd residualPc = accountedPc.not();
+
+        int i = 0;
+        for (PrimVS choice : choices) {
+            if (residualPc.isConstFalse()) break;
+            Bdd enabledCond = choice.getUniverse();
+            PrimVS guarded = choice.guard(residualPc);
+            results.add(guarded);
+            residualPc = residualPc.and(enabledCond.not());
+        }
+        return empty.merge(results);
+    }
+
+    public static PrimVS getNondetChoice(List<PrimVS> choices) {
+        if (choices.size() == 0) return new PrimVS<>();
+        List<PrimVS> results = new ArrayList<>();
+        PrimVS empty = choices.get(0).guard(Bdd.constFalse());
+        List<Bdd> choiceVars = new ArrayList<>();
+
+        ValueSummaryUnionFind uf = new ValueSummaryUnionFind(choices);
+        Collection<Set<PrimVS>> disjoint = uf.getDisjointSets();
+        // only need to distinguish between things within disjoint sets
+        Map<Set<PrimVS>, Bdd> universeMap = uf.getLastUniverseMap();
+
+        int maxSize = 0;
+        for (Set<PrimVS> set : disjoint) {
+            int size = set.size();
+            if (size > maxSize) {
+                maxSize = size;
+            }
+        }
+
+        int numVars = 1;
+        while ((1 << numVars) - maxSize < 0) {
+            numVars++;
+        }
+
+        for (int i = 0; i < numVars; i++) {
+            choiceVars.add(Bdd.newVar());
+        }
+
+        List<Bdd> choiceConds = generateAllCombos(choiceVars);
+
+        for (Set<PrimVS> set : disjoint) {
+            results.addAll(getIntersectingNondetChoice(new ArrayList<>(set), choiceConds, universeMap.get(set)));
+        }
+
+        return empty.merge(results);
+    }
+
+    public static List<PrimVS> getIntersectingNondetChoice(List<PrimVS> choices, List<Bdd> choiceConds, Bdd universe) {
+        List<PrimVS> results = new ArrayList<>();
+        Bdd accountedPc = Bdd.constFalse();
+        for (int i = 0; i < choices.size(); i++) {
+            PrimVS choice = choices.get(i).guard(choiceConds.get(i));
+            results.add(choice);
+            accountedPc = accountedPc.or(choice.getUniverse());
+        }
+
+        Bdd residualPc = accountedPc.not().and(universe);
+
         for (PrimVS choice : choices) {
             if (residualPc.isConstFalse()) break;
             Bdd enabledCond = choice.getUniverse();
@@ -77,52 +123,7 @@ public class NondetUtil {
             residualPc = residualPc.and(enabledCond.not());
         }
 
-        return empty.merge(results);
+        return results;
     }
 
-    public static PrimVS getNondetChoiceAlt(List<PrimVS> choices) {
-        if(choices.size() == 0) return new PrimVS<>();
-        List<PrimVS> results = new ArrayList<>();
-        PrimVS empty = choices.get(0).guard(Bdd.constFalse());
-
-        Bdd residualPc = Bdd.constTrue();
-        for (PrimVS choice : choices) {
-            assert(!residualPc.isConstFalse());
-            Bdd enabledCond = choice.getUniverse();
-            Bdd choiceCond = Bdd.newVar().and(enabledCond);
-            assert(!choiceCond.isConstFalse());
-            if (choiceCond.isConstFalse()) {
-                throw new RuntimeException();
-            }
-
-            //System.out.println("residual pc: " + residualPc);
-            //System.out.println("choiceCond: " + choiceCond);
-            Bdd returnPc = residualPc.and(choiceCond);
-            assert(!returnPc.isConstFalse());
-            results.add(choice.guard(returnPc));
-
-            //System.out.println("new residual pc");
-            //System.out.println("negated choice cond: " + choiceCond.not());
-            residualPc = residualPc.and(choiceCond.not());
-        }
-
-        for (PrimVS choice : choices) {
-            Bdd enabledCond = choice.getUniverse();
-
-            Bdd returnPc = residualPc.and(enabledCond);
-            results.add(choice.guard(returnPc));
-
-            if (!choice.guard(returnPc).getUniverse().isConstFalse()) {
-                ScheduleLogger.log("not false");
-            }
-
-            residualPc = residualPc.and(enabledCond.not());
-        }
-
-        final Bdd noneEnabledCond = residualPc;
-        PrimVS<Boolean> isPresent = BoolUtils.fromTrueGuard(noneEnabledCond.not());
-
-        assert(Checks.sameUniverse(noneEnabledCond.not(), empty.merge(results).getUniverse()));
-        return empty.merge(results);
-    }
 }
